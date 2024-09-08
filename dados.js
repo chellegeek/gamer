7dd98378aa7b238af4eb796b699c982bb2a7ff1d
Michelle Garcia <112138416+chellegeek@users.noreply.github.com>
2024-09-07 22:42:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+// dados.js
+//export 
+let dados = [
+{
+    titulo: "Atari 2600",
+    descricao: "Atari 2600, originalmente vendido como Atari Video Computer System ou Atari VCS até novembro de 1982, é um videogame projetado por Jay Miner e lançado em 11 de setembro de 1977 nos Estados Unidos e em 1983 no Brasil. Sendo descontinuado oficialmente em 1992.",
+    link: "https://pt.wikipedia.org/wiki/Atari_2600"
+},
+{
+    titulo: "Sega Saturn",
+    descricao: " O Sega Saturn é um console de jogos eletrônicos de quinta geração, lançado pela empresa Sega em 22 de Novembro de 1994 no Japão, 11 de Maio de 1995 na América do Norte, 30 de agosto de 1995 no Brasil e 8 de Julho na Europa. Foi o sucessor do bem sucedido Sega Genesis. O Saturn tem uma arquitetura de CPU dupla e oito processadores. Seus jogos utilizam a mídia CD-ROM e sua biblioteca de jogos contou com várias portes de jogos de arcade e também jogos originais desenvolvidos para o console.",
+    link: "https://pt.wikipedia.org/wiki/Sega_Saturn"
+},
+{
+    titulo: "Game Boy",
+    descricao: "É um console portátil desenvolvido pela Nintendo, lançado em 21 de abril de 1989 no Japão, em 31 de julho de 1989 na América do Norte e em 28 de setembro de 1990 na Europa, é o primeiro console da linha Game Boy, foi criado por Gunpei Yokoi e pela Nintendo Research & Development 1, versões redesenhadas do console foram lançadas em 1996 e em 1998, o Game Boy Pocket e o Game Boy Light (somente para o Japão)",
+    link: "https://pt.wikipedia.org/wiki/Game_Boy"
+},
+{
+    titulo: "Super Nintendo Entertainment System",
+    descricao: "O Super Nintendo Entertainment System (ou apenas Super Nintendo, abreviado SNES; no Japão Super Famicom, abreviado SFC, em japonês: スーパーファミコン, transl. Sūpā Famikon ) é um console de videogame de 16-bits da quarta geração desenvolvido pela Nintendo, que foi lançado no período de 1990 à 1993 em várias parte do mundo. No Japão foi adotando o nome abreviado do seu antecessor, o Famicom (de Nintendo Family Computer) sendo chamado Super Famicom. Na Coreia do Sul, é conhecido como Super Comboy e foi distribuído pela Hyundai Electronics. Embora cada versão seja essencialmente as mesmas, várias formas de bloqueio regional impedem que as diferentes versões sejam compatíveis entre si.",
+    link: "https://pt.wikipedia.org/wiki/Super_Nintendo_Entertainment_System"
+},
+{
+    titulo: "Mega Drive",
+    descricao: "Conhecido como Sega Genesis na América do Norte, é um console de videogame de 16 bits desenvolvido e comercializado pela Sega sendo um sucessor do Master System. O console foi lançado em 1988 no Japão como Mega Drive e em 1989 na América do Norte como Genesis. No ano de 1990, foi distribuído na Europa pela Virgin Mastertronic, na Nova Zelândia e Austrália pela Ozisoft e pela Tec Toy no Brasil. Na Coreia do Sul, foi distribuído pela Samsung com o nome Super Gam*Boy e posteriormente como Super Aladdin Boy. Na Rússia, foi distribuído pela Forrus.[9]",
+    link: "https://pt.wikipedia.org/wiki/Mega_Drive"
+},
+{
+    titulo: "Neo Geo CD",
+    descricao: "O Neo Geo CD (ネオジオ Neo Jio Shī Dī?) é um console de videogame lançado na Tokyo Toy Show em 1994.[1] O console usa a mesma configuração de CPU que os sistemas Neo Geo baseados em cartucho, e arcades, facilitando conversões. A SNK planejava lançar versões de Neo Geo CD para cada jogo Neo Geo ainda em uso em arcades.",
+    link: "https://pt.wikipedia.org/wiki/Neo_Geo_CD"
+},
+{
+    titulo: "Nintendo 64",
+    descricao: "Nintendo 64 (com a grafia estilizada NINTENDO64, e frequentemente abreviado de N64), é um console de jogos eletrônicos da quinta geração lançado pela Nintendo, em 23 de junho de 1996 no Japão. Lançado como a quinta geração de consoles da Nintendo, foi uma revolução no mercado da época, graças à sua arquitetura de 64 bits, que permitiu introduzir 3D nos jogos, tornando-os mais imersíveis.",
+    link: "https://pt.wikipedia.org/wiki/Nintendo_64"
+},
+{
+    titulo: "Atari Jaguar",
+    descricao: "Atari Jaguar é um console de videogame da quinta geração com 64 bits, lançado pela empresa Flare II (Atari Corporation) em 1993, para concorrer com os consoles Mega Drive e Super Nintendo em poder de processamento.Disputou mercado com o console 3DO e posteriormente com os videogames da 5ª geração.",
+    link: "https://pt.wikipedia.org/wiki/Atari_Jaguar"
+}
+]
